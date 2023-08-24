38ce62659a14773a93cd577227c2d74890608ee7
jakobpalmborg <jakob.palmborg@gmail.com>
2023-08-24 15:54:56 +0200

    create fileInput class and add to menu and create csv file

---
+++ src/main/java/org/example/FileInput.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.example.Menu.menu;
+
+public class FileInput {
+    public static void fileInput() {
+        System.out.println("this is file input");
+
+        menu();
+    }
+}
--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -11,6 +11,7 @@ public class Menu {
         System.out.println("2. Min, Max och Medel");
         System.out.println("3. Sortera");
         System.out.println("4. Bästa Laddningstid (4h)");
+        System.out.println("5. Inläsning Från Fil");
         System.out.println("e. Avsluta");
 
         Scanner scanner = new Scanner(System.in);
@@ -22,6 +23,7 @@ public class Menu {
         else if (menuChoice.equals("2")) MinMaxAverage.minMaxAverage();
         else if (menuChoice.equals("3")) Sort.sort();
         else if (menuChoice.equals("4")) ChargeTime.chargeTime();
+        else if (menuChoice.equals("5")) FileInput.fileInput();
         else {
             System.out.println("inte ett giltigt val, försök igen");
             menu();
