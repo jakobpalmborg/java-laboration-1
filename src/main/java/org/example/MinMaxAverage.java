12b62e80b1072257992d93f0d5a6979154da9d6f
jakobpalmborg <jakob.palmborg@gmail.com>
2023-08-25 14:38:36 +0200

    show all hours with min and max price

--- src/main/java/org/example/MinMaxAverage.java
+++ src/main/java/org/example/MinMaxAverage.java
@@ -9,6 +9,26 @@ public class MinMaxAverage {
         if (App.hourPriceArray[0] == null) {
             System.out.println("Inmatning av värden saknas. Gå till menyval 1 för att mata in värden.");
         } else {
+            // Min price
+            int minValue = 2147483647;
+            int indexOfMinValue = -1;
+            for (int i = 0; i < App.hourPriceArray.length; i++) {
+                if (App.hourPriceArray[i].price < minValue) {
+                    indexOfMinValue = i;
+                    minValue = App.hourPriceArray[i].price;
+                }
+            }
+
+            System.out.println("Minpris: " + App.hourPriceArray[indexOfMinValue].price + " öre");
+            System.out.print("Minpris kl: ");
+            for (int i = 0; i < App.hourPriceArray.length; i++) {
+                if (App.hourPriceArray[i].price == minValue) {
+                    System.out.print(App.hourPriceArray[i].hour + ", ");
+                }
+            }
+            System.out.println();
+
+            // Max price
             int maxValue = -1;
             int indexOfMaxValue = -1;
 
@@ -18,25 +38,23 @@ public class MinMaxAverage {
                     maxValue = App.hourPriceArray[i].price;
                 }
             }
-            System.out.println("max: " + " kl: " + App.hourPriceArray[indexOfMaxValue].hour + " pris: " + App.hourPriceArray[indexOfMaxValue].price + " öre");
-
-            int minValue = 2147483647;
-            int indexOfMinValue = -1;
+            System.out.println("Maxpris: " + App.hourPriceArray[indexOfMaxValue].price + " öre");
+            System.out.print("Maxpris kl: ");
             for (int i = 0; i < App.hourPriceArray.length; i++) {
-                if (App.hourPriceArray[i].price < minValue) {
-                    indexOfMinValue = i;
-                    minValue = App.hourPriceArray[i].price;
+                if (App.hourPriceArray[i].price == maxValue) {
+                    System.out.print(App.hourPriceArray[i].hour + ", ");
                 }
             }
-            System.out.println("min: " + " kl: " + App.hourPriceArray[indexOfMinValue].hour + " pris: " + App.hourPriceArray[indexOfMinValue].price + " öre");
+            System.out.println();
 
+            // Average price
             int average;
             int total = -1;
             for (var hour : App.hourPriceArray) {
                 total += hour.price;
             }
             average = total / 24;
-            System.out.println("medel: " + average + " öre");
+            System.out.println("Medelpris: " + average + " öre / timme");
         }
 
         Menu.menu();
