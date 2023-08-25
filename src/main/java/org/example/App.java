7a723a88d88a9e49bd56bb186a373b9bc5d05585
jakobpalmborg <jakob.palmborg@gmail.com>
2023-08-25 10:18:11 +0200

    read from csv file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,8 @@ public class App {
     public static HourPrice[] hourPriceArray = new HourPrice[24];
     public static void main(String[] args) {
 
-        menu();
+Menu.menu();
+
     }
 }
 
--- src/main/java/org/example/FileInput.java
+++ src/main/java/org/example/FileInput.java
@@ -1,11 +1,37 @@
 package org.example;
 
-import static org.example.Menu.menu;
+import java.io.*;
+
 
 public class FileInput {
     public static void fileInput() {
-        System.out.println("this is file input");
+        System.out.println("Inläsning Från Fil");
+        System.out.println("========");
 
-        menu();
+        String file = "src/main/resources/priser.csv";
+        BufferedReader reader = null;
+        String line = "";
+        int index = 0;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                HourPrice hourPrice = new HourPrice();
+                hourPrice.hour = row[0];
+                hourPrice.price = Integer.parseInt(row[1]);
+                App.hourPriceArray[index] = hourPrice;
+                index++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Menu.menu();
     }
 }
