00dc05dda42d771d4870b5c14bbe279d459f43d0
jakobpalmborg <jakob.palmborg@gmail.com>
2023-08-29 15:06:22 +0200

    try catch for data input scanner

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,15 @@
 package org.example;
 
 public class
+
+
 App {
     public static HourPrice[] hourPriceArray = new HourPrice[24];
     public static void main(String[] args) {
 
 Menu.menu();
+
+
 
     }
 }
--- src/main/java/org/example/Input.java
+++ src/main/java/org/example/Input.java
@@ -11,13 +11,17 @@ public class Input {
         System.out.println("Fyll i priser i öre för varje timme");
 
         Scanner scanner = new Scanner(System.in);
-
-        for (int i = 0; i < 24; i++) {
-            HourPrice hourPrice = new HourPrice();
-            hourPrice.hour = (i < 10 ? "0" + i + ":00-" : i + ":00-") + (i == 23 ? "00:00" : i < 9 ? "0" + (i + 1) + ":00" : (i + 1) + ":00");
-            System.out.println(hourPrice.hour);
-            hourPrice.price = scanner.nextInt();
-            App.hourPriceArray[i] = hourPrice;
+        try {
+            for (int i = 0; i < 24; i++) {
+                HourPrice hourPrice = new HourPrice();
+                hourPrice.hour = (i < 10 ? "0" + i + ":00-" : i + ":00-") + (i == 23 ? "00:00" : i < 9 ? "0" + (i + 1) + ":00" : (i + 1) + ":00");
+                System.out.println(hourPrice.hour);
+                hourPrice.price = scanner.nextInt();
+                App.hourPriceArray[i] = hourPrice;
+            }
+        } catch (Exception e) {
+            System.out.println("Fel inmatning. Använd siffor 0-9.");
+            input();
         }
 
         Menu.menu();
